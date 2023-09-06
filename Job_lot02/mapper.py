efcff8409a78fe032c750cd56e5c1798691d59de
NGendre <noe.gendre@outlook.fr>
2023-09-06 15:16:45 +0200

    ready for demo

--- Job_lot02/mapper.py
+++ Job_lot02/mapper.py
@@ -1,15 +1,94 @@
 #!/usr/bin/env python
 """mapper.py"""
-
+import json
 import sys
+from typing import Set, Any, List
+
+import numpy as np
 import pandas as pd
+from dateutil.parser import parse
 from pandas import DataFrame
 
-sys.path.insert(0,"..")
-from classes.CSV_Cleaner import LineCleaner
-from classes.CSV_HeaderChecker import HeaderChecker
-from classes.CSV_Mapper import Mapper
 
+class Mapper:
+
+    def __init__(self, set_headers: Set[str]):
+        self.__correct_headers = set_headers
+        self.__dico = {}
+
+    def filter(self, dataframe: DataFrame):
+        filtered_dataframe = dataframe.filter(items=self.__correct_headers)
+        self.__dico = filtered_dataframe.iloc[0].to_dict()
+        return self
+
+    def to_json(self) -> str:
+        json_ligne = json.dumps(self.__dico,default=convert_int64_to_int)
+        return json_ligne
+
+    def to_raw_str(self) -> str:
+        valeurs = []
+        for valeur in self.__dico.values():
+            valeurs.append(valeur)
+        resultat = ', '.join(valeurs)
+        return resultat
+
+class HeaderChecker:
+
+    def __init__(self, set_headers: Set[str]):
+        self.__is_header = True
+        self.__correct_headers = set_headers
+
+    def is_header(self) -> bool:
+        return self.__is_header
+
+    def is_not_correct(self, words_array: List[str]) -> bool:
+        self.__is_header = False
+        for header in self.__correct_headers:
+            if header not in words_array:
+                return True
+        return False
+
+class LineCleaner:
+    @staticmethod
+    def delimit(line: str, delimiter: str) -> List[str]:
+        line = line.strip()
+        return line.split(delimiter)
+
+    @staticmethod
+    def clean_double_quote_if_not_empty(words: List[str]) -> None:
+        for (index, word) in enumerate(words):
+            if word != "" and word[0] == '"' and word[-1] == '"':
+                word = word[1:]
+                word = word[:-1]
+                words[index] = word
+
+    @staticmethod
+    def has_clean_date(dataframe: DataFrame) -> bool:
+        valid_date = dataframe['datcde'].apply(is_valid_date)
+        return valid_date.bool()
+
+    @staticmethod
+    def is_between_years(dataframe: DataFrame,startYear:int,endYear:int) -> bool:
+        date = pd.to_datetime(dataframe['datcde'],errors='coerce').dt.year
+        try:
+            date = int(date)
+            if date >= startYear and date <= endYear:
+                return True
+            return False
+        except:
+            return False
+
+def is_valid_date(value: Any):
+    try:
+        parse(str(value), fuzzy=True, dayfirst=True)
+        return True
+    except ValueError:
+        return False
+
+def convert_int64_to_int(obj):
+    if isinstance(obj, np.int64):
+        return int(obj)
+    return obj
 
 headers = {
     "cpcli",
@@ -24,8 +103,12 @@ headers = {
 header_checker = HeaderChecker(headers)
 mapper = Mapper(headers)
 df = None
+# counter = 0
 
 for line in sys.stdin:
+    # counter += 1
+    # if counter>20000:
+    #     break
     words = LineCleaner.delimit(line, ",")
     LineCleaner.clean_double_quote_if_not_empty(words)
     if header_checker.is_header():
@@ -33,7 +116,10 @@ for line in sys.stdin:
             break
         df = DataFrame(columns=words)
         continue
-    df.loc[0] = words  # insert row in dataframe
+    try:
+        df.loc[0] = words  # insert row in dataframe
+    except:
+        continue
     if not LineCleaner.has_clean_date(df):
         continue
     if not LineCleaner.is_between_years(df,2006,2016):
